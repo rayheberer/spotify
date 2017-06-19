a546cc82084c609307484948d3d9269ce07df111
Ray Heberer <ray.heberer@gmail.com>
2017-06-19 21:58:42 +0200

    data wrangling finished

--- exploratory data analysis.R
+++ exploratory data analysis.R
@@ -37,13 +37,50 @@ top.song.ids = substr(daily.top.songs, id.start+7, id.end-1)
 # API requests
 
 # Get track
-track = GET(url = paste("https://api.spotify.com/v1/tracks/", top.song.ids[1], sep = ""),
+tracks = lapply(1:200, function(n) {
+  GET(url = paste("https://api.spotify.com/v1/tracks/", top.song.ids[n], sep = ""),
                         config = add_headers(authorization = authorization.header))
+})
 
-# Audio Analysis
-analysis = GET(url = paste0("https://api.spotify.com/v1/audio-analysis/", top.song.ids[1]),
-           config = add_headers(authorization = authorization.header))
+tracks.content = sapply(1:200, function(n) {
+  content(tracks[[n]])
+})
+
+tracks.content = t(tracks.content)
+tracks.names = sapply(1:200, function(n) {
+  tracks.content[[n]]$name
+})
+
+tracks.df = cbind(rating = 1:200, name = tracks.names)
+tracks.df = tracks.df %>% as.data.frame
 
 # Audio Features
-features = GET(url = paste0("https://api.spotify.com/v1/audio-features/", top.song.ids[1]),
+features = lapply(1:200, function(n) {
+  GET(url = paste0("https://api.spotify.com/v1/audio-features/", top.song.ids[n]),
                config = add_headers(authorization = authorization.header))
+})
+
+features.content = sapply(1:200, function(n) {
+  content(features[[n]])
+})
+
+features.content = t(features.content)
+
+features.df = cbind(rating = 1:200, danceability = features.content[,1], 
+                    energy = features.content[,2], key = features.content[,3], 
+                    loudness = features.content[,4], mode = features.content[,5],
+                    speechiness = features.content[,6], acousticness = features.content[,7],
+                    instrumentalness = features.content[,8], liveness = features.content[,9],
+                    valence = features.content[,9], tempo = features.content[,10], 
+                    duration_ms = features.content[,17], time_signature = features.content[,18])
+
+features.df = features.df %>% as.data.frame
+
+
+
+
+# Audio Analysis
+#analyses = lapply(1:200, function(n) {
+#  GET(url = paste0("https://api.spotify.com/v1/audio-analysis/", top.song.ids[n]),
+#           config = add_headers(authorization = authorization.header))
+#})
