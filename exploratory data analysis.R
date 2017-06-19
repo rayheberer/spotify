21235dd0bccae7b602e4e6672bbea199f22036e3
Ray Heberer <ray.heberer@gmail.com>
2017-06-19 20:46:09 +0200

    regenerated keys and put into non-public file.
    authorization and API figured out

--- exploratory data analysis.R
+++ exploratory data analysis.R
@@ -1,15 +1,49 @@
 # ///////////////////
-# libraries
+# LIBRARIES
 library(httr)
-
+library(magrittr)
+library(rvest)
+library(jsonlite)
 # //////////////////
+clientID = ""
+secret = ""
+
+response = POST(
+  'https://accounts.spotify.com/api/token',
+  accept_json(),
+  authenticate(clientID, secret),
+  body = list(grant_type = 'client_credentials'),
+  encode = 'form',
+  verbose()
+)
+
+token = content(response)$access_token
+
+authorization.header = paste0("Bearer ", token)
+#GET(url = paste("https://api.spotify.com/v1/tracks/", ":ID", sep = ""),
+#           config = add_headers(authorization = authorization.header))
+# //////////////////
+# WEB SCRAPING
 
 
-spotify.key = "e94b7f19fe754828bc335fd2fc360bd5"
-spotify.secret = "41ee9eea78224541b425522312e60b26"
+daily.top.songs = read_html("https://spotifycharts.com/regional/global/daily/latest") %>%
+  html_nodes("#content > div > div > div > span > table > tbody > tr > td.chart-table-image > a")
 
-spotify.endpoint = oauth_endpoint(NULL, 
-                                  "https://accounts.spotify.com/authorize",
-                                  "hyyps://acounts.spotify.com/api/token")
+id.start = regexpr("/track/", daily.top.songs) # seems to always be 7
+id.end = regexpr('" target="', daily.top.songs) # seems to always be 63
 
-spotify.app = oauth_app("spotify", spotify.key, spotify.secret)
+top.song.ids = substr(daily.top.songs, id.start+7, id.end-1)
+# //////////////////
+# API requests
+
+# Get track
+track = GET(url = paste("https://api.spotify.com/v1/tracks/", top.song.ids[1], sep = ""),
+                        config = add_headers(authorization = authorization.header))
+
+# Audio Analysis
+analysis = GET(url = paste0("https://api.spotify.com/v1/audio-analysis/", top.song.ids[1]),
+           config = add_headers(authorization = authorization.header))
+
+# Audio Features
+features = GET(url = paste0("https://api.spotify.com/v1/audio-features/", top.song.ids[1]),
+               config = add_headers(authorization = authorization.header))
