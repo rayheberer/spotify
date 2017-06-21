b5a47fed3f8b17258976e673b16d530f13318102
Ray Heberer <ray.heberer@gmail.com>
2017-06-21 15:42:14 +0200

    added data for viral songs

--- web_scraping.R
+++ web_scraping.R
@@ -13,19 +13,19 @@ urls.viral = sapply(1:days, function(n) {
   paste0("https://spotifycharts.com/viral/global/daily/", as.character(date))
 })
 
-daily.top.songs = lapply(urls.top[401:500], function(url) {
+daily.viral.songs = lapply(urls.viral[1:100], function(url) {
   read_html(url) %>%
   html_nodes("#content > div > div > div > span > table > tbody > tr > td.chart-table-image > a")
 })
 
-id.starts = lapply(daily.top.songs, function(day) {
+id.starts = lapply(daily.viral.songs, function(day) {
   regexpr("/track/", day)
 })
 
-id.ends = lapply(daily.top.songs, function(day) {
+id.ends = lapply(daily.viral.songs, function(day) {
   regexpr('" target="', day)
 })
 
-daily.top.ids.301_400 = lapply(1:100, function(n) {
-  substr(daily.top.songs[[n]], id.starts[[n]]+7, id.ends[[n]]-1)
+daily.viral.ids.1_100 = lapply(1:100, function(n) {
+  substr(daily.viral.songs[[n]], id.starts[[n]]+7, id.ends[[n]]-1)
 })
