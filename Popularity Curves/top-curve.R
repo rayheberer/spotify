41016ba0d1f75eb6def33fccf7e719dc877f1f4e
Ray Heberer <ray.heberer@gmail.com>
2017-06-22 18:29:38 +0200

    shiny app template made

--- Popularity Curves/top-curve.R
+++ Popularity Curves/top-curve.R
@@ -1,5 +1,6 @@
 library(tidyr)
 library(magrittr)
+library(ggplot2)
 
 match_number = sapply(daily.top.ids.all, function(day) {
   match("4IIUaKqGMElZ3rGtuvYlNc", day)
@@ -14,9 +15,11 @@ top.curves = lapply(daily.top.ids.all[[1]], function(id) {
           as.Date("2017-06-20")-n+1,
           201 - match(id, daily.top.ids.all[[n]]))
   })
-  df = data.frame(data = vector) %>% 
+  
+  df = data.frame(data = vector, stringsAsFactors = FALSE) %>% 
     separate(data, c("position", "id", "date", "popularity"), sep = " ")
 })
 
 
 
+ggplot(top.curves[[2]], aes(x = date, y = position)) + geom_point()
---
+++ ShinyApp/app.R
@@ -0,0 +1,30 @@
+library(shiny)
+
+ui = fluidPage(
+  
+  sidebarLayout(
+    
+    sidebarPanel(
+      dateInput(inputId = "date",
+                label = "Date",
+                value = "2017-06-20",
+                min = "2016-05-17",
+                max = "2017-06-20"),
+      sliderInput(inputId = "rank",
+                  label = "Top Chart Ranks Included",
+                  value = c(1,200),
+                  min = 1,
+                  max = 200)
+      
+    ),
+    
+    mainPanel(
+      
+    )
+  )
+  
+)
+
+server = function(input, output) {}
+
+shinyApp(ui = ui, server = server)
